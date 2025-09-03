742b831aade426d34a8e2fe01c26d9d7c1f344b4
TellusCZ <81971333+TellusCZ@users.noreply.github.com>
2025-09-03 15:12:02 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -533,6 +533,23 @@ document.getElementById('saveScriptPng').addEventListener('click', () => {
     });
   });
 
+$("inputImage").change(function(e) {
+
+    for (var i = 0; i < e.originalEvent.srcElement.files.length; i++) {
+        
+        var file = e.originalEvent.srcElement.files[i];
+        
+        var img = document.createElement("img");
+        var reader = new FileReader();
+        reader.onloadend = function() {
+             img.src = reader.result;
+        }
+        reader.readAsDataURL(file);
+        $("input").after(img);
+    }
+});
+
+
 
 
 
