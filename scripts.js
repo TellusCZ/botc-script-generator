f3783be172550ba80f0b0bd111d45a683dad6839
TellusCZ <81971333+TellusCZ@users.noreply.github.com>
2025-09-11 16:30:54 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -521,7 +521,7 @@ let html = '';
     //html += "<div>" + val + "</div>";
   });
   html += "</div></div>";
-  html += "<div class='nightorderpanel'>" + "pokus" + </div>;
+  html += "<div class='nightorderpanel'>" + "pokus" + "</div>";
   html += "</div>";
   document.getElementsByClassName('OtherNightOrderImage')[0].innerHTML = html;
 //});
@@ -629,6 +629,7 @@ roleHeightslider.addEventListener("input", updateHeight);
 TownsfolkOffsetSlider.addEventListener("input", updateTownsfolkSlider);
 MinionOffsetSlider.addEventListener("input", updateMinionSlider);
 DemonOffsetSlider.addEventListener("input", updateDemonSlider);
+
 
 
 
