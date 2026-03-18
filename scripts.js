b73f7253faa82bafec0746573ffb939ff09f72a6
TellusCZ <81971333+TellusCZ@users.noreply.github.com>
2026-03-18 23:11:14 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -639,8 +639,8 @@ function renderJinxTableWithImages(idObjects, jinxData, rolesData, tableId) {
                     else
                         html += "style='color:red;'";
                     html += ">" + roleMap[first]?.name;// + "<br>";
-                html += " & "
-                html += "<p;
+                html += " & ";
+                html += "<p";
                     if (roleMap[second]?.team == "townsfolk" || roleMap[second]?.team == "outsider")
                         html += "style='color:blue;'";
                     else
